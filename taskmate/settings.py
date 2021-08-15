3d9ab04d03541ed96b350a65385369e48ee5faab
Suraj0624 <surajs0624@gmail.com>
2021-08-15 19:56:55 +0530

    Project upd

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 import environ
 
@@ -134,3 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
